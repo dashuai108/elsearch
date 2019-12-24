9ce4b39ab022cef2820e7221c1c4ea77759aa8d0
dashuai108 <36142946+dashuai108@users.noreply.github.com>
2019-12-24 10:00:58 +0800

    Update ApplicationContextHolder.java

--- src/main/java/com/example/myelastisearch/config/ApplicationContextHolder.java
+++ src/main/java/com/example/myelastisearch/config/ApplicationContextHolder.java
@@ -4,7 +4,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
-
+import javax.annotation.PostConstruct;
 @Component
 public class ApplicationContextHolder implements ApplicationContextAware {
 
@@ -14,6 +14,7 @@ public class ApplicationContextHolder implements ApplicationContextAware {
         ApplicationContextHolder.applicationContext = applicationContext;
     }
 
+    @PostConstruct
     public  static <T>T getBean(String beanName){
         return (T)applicationContext.getBean(beanName);
     }
